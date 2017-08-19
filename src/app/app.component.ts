c6ac8c969bebb533595283bfd6b07f6bda01bc63
Ankit Kumar <ankitbansal.nitj@gmail.com>
2017-08-19 20:22:16 +0530

    Network Status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform, ModalController } from 'ionic-angular';
+import { Nav, Platform, ModalController, LoadingController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Network } from '@ionic-native/network';
 import { HomePage } from '../pages/home/home'
 import { AboutPage } from '../pages/about/about';
 import { MenuPage } from '../pages/menu/menu';
@@ -19,11 +19,14 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
+  loading: any = null;
 
   pages: Array<{title: string, icon: string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
-     public modalController: ModalController) {
+     public modalController: ModalController,
+     private loadingCtrl: LoadingController,
+    private network: Network) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -44,6 +47,31 @@ export class MyApp {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+
+    this.network.onDisconnect().subscribe(() => {
+        if (!this.loading) {
+          this.loading = this.loadingCtrl.create({
+            content: 'Network Disconnected'
+          });
+          this.loading.present();
+        }
+      });
+
+      this.network.onConnect().subscribe(() => {
+
+        // We just got a connection but we need to wait briefly
+        // before we determine the connection type. Might need to wait.
+        // prior to doing any api requests as well.
+        setTimeout(() => {
+          if (this.network.type === 'wifi') {
+            console.log('we got a wifi connection, woohoo!');
+          }
+        }, 3000);
+        if (this.loading) {
+          this.loading.dismiss();
+          this.loading = null;
+        }
+      });
   }
 
   openPage(page) {
